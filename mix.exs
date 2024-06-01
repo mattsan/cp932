a31c3b737c1f501588aa04c9e1df12ff0d33f87f
MATSUMOTO Eiji <e.mattsan@gmail.com>
2024-06-01 17:04:18 +0900

    Add packages (#1)
    * add Codepagex to convert string encodings
    
    * add dev tools

--- mix.exs
+++ mix.exs
@@ -11,18 +11,17 @@ defmodule CP932.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:codepagex, "~> 0.1.9"},
+      {:ex_doc, "~> 0.34.0", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: :dev, runtime: false}
     ]
   end
 end
